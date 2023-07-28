e3c47bf52edde183c794712f6b8d2f214c248da7
Konstantin Grudnitskiy <k.grudnitskiy@yandex.ru>
2023-07-28 19:03:45 +0300

    simple errorHandler added

--- index.ts
+++ index.ts
@@ -5,51 +5,70 @@ import GreetArtifact from "./build/contracts/Greet.json";
 
 dotenv.config();
 
-const web3 = new Web3('ws://127.0.0.1:8545');
+const web3 = new Web3('ws://127.0.0.1:8545'); //Replace with your RPC
 const GreetContract = new web3.eth.Contract(
     GreetArtifact.abi,
     GreetArtifact.networks[process.env.NETWORK_ID].address,
-  );
+);
 
-GreetContract.events.allEvents().on('data', (event) => {
-    console.log(`
-  ${event.event} event received.
-  Transaction Hash: ${event.transactionHash}
-  Signature: ${event.signature}
-`);
-})
+async function watch(contract: Contract<ContractAbi>, events: string) {
+    contract.events[events]().on('data', (event) => {
+        console.log(`
+      ${event.event} event received.
+      Transaction Hash: ${event.transactionHash}
+      Signature: ${event.signature}
+    `);
+    })
+}
 
 async function interact(contract: Contract<ContractAbi>) {
     const providersAccounts = await web3.eth.getAccounts();
     const defaultAccount = providersAccounts[0];
 
-    try {
-        const curGreet = await contract.methods.greet().call();
-        console.log('Cur greet:', curGreet);
-        console.log(await getGreetings(contract, "Konstantin"));
+    const curGreet = await contract.methods.greet().call();
+    console.log('Cur greet:', curGreet);
+    console.log(await getGreetings(contract, "Konstantin"));
 
-        await setGreet(contract, defaultAccount, "Hi");
-        const newGreet = await contract.methods.greet().call();
-        console.log('New greet:', newGreet);
-        console.log(await getGreetings(contract, "Johnatan"));
+    await setGreet(contract, defaultAccount, "Hi");
+    const newGreet = await contract.methods.greet().call();
+    console.log('New greet:', newGreet);
+    console.log(await getGreetings(contract, "Johnatan"));
 
-        await setGreet(contract, defaultAccount, "Hello");
-    } catch (error) {
-        console.error(error);
-    }
+    await setGreet(contract, defaultAccount, "Hello"); // reset the value
 }
 
-async function setGreet(contract, account, value) {
+
+async function setGreet(contract, account: string, value: string) {
     const receipt = await contract.methods.setGreet(value).send({
         from: account,
-        gas: '1000000',
+        gas: '100000',
         gasPrice: '10000000000',
     });
     console.log('setGreet transaction hash:', receipt.transactionHash);
 }
 
-async function getGreetings(contract, username) {
+
+async function getGreetings(contract, username: string) {
     return await contract.methods.greetings(username).call();
 }
 
-interact(GreetContract);
+async function errorHandler(runner: string, reason: Error) {
+    let msg: string = '';
+    switch (reason.name) {
+        case 'AbiError':
+            msg = `Check please that your contract exists in the ${process.env.NETWORK_ID} network.`;
+            break;
+
+        default:
+            msg = `Something went wrong: ${reason}`;
+            break;
+    }
+    console.log(`[${runner}] ${msg}`);
+}
+
+watch(GreetContract, 'allEvents').catch(
+    (reason) => errorHandler('watch', reason)
+);
+interact(GreetContract).catch(
+    (reason) => errorHandler('interact', reason)
+);
